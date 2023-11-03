3fbebc75a6cb83096bab2af5d5818bb1aab217e2
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-11-03 19:27:14 +0545

    Add blog

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require("express")
+const express = require("express");
+const { blogs } = require("./model/index.js");
 const app = express()
 
 // tell nodejs to require and use .env
  require("dotenv").config()
 
+require("./model/index.js");
+
 // tell nodejs that we are using ejs, set everything
 app.set("view engine","ejs")
+
+//telling nodejs to accept the incoming data(parsing data)
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get("/",(req,res)=>{
     res.render("allBlogs")
@@ -13,6 +20,17 @@ app.get("/",(req,res)=>{
 
 app.get("/addBlog",(req,res)=>{
     res.render("addBlog")
+})
+
+//apo for handling formdata
+app.post("addBlog",async (req,res)=>{
+    // console.log("API hitted",req.body)
+     await blogs.create({
+        title:req.body.title,
+        subTitle:req.body.subtitle,
+        description:req.body.description
+    })
+    res.send("Blog created successfully")
 })
 
 const PORT  = process.env.PORT
