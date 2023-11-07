055ba3cad8c8798f627b3efa00a150438401e7a9
bsna-rjt <basnaranjitkar1138@gmail.com>
2023-11-07 18:46:11 +0545

    View Single Blog in See More click, DELETE selected blog

--- app.js
+++ app.js
@@ -2,67 +2,99 @@ const express = require("express");
 const { blogs } = require("./model/index.js");
 
 //requiring multerConfig
-const {multer,storage} = require("./middleware/multerConfig.js");
+const { multer, storage } = require("./middleware/multerConfig.js");
 // const multer = require("./middleware/multerConfig.js").multer;
 // const storage = require("./middleware/multerConfig.js").storage;
-const upload =  multer({storage:storage})
+const upload = multer({ storage: storage });
 
-const app = express()
+const app = express();
 
 // tell nodejs to require and use .env
- require("dotenv").config()
+require("dotenv").config();
 
 require("./model/index.js");
 
 // tell nodejs that we are using ejs, set everything
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 
 //telling nodejs to accept the incoming data(parsing data)
-app.use(express.json()) //content type = application/json handle
-app.use(express.urlencoded({extended:true})) //content type = application/x-www-form-urlencoded
+app.use(express.json()); //content type = application/json handle
+app.use(express.urlencoded({ extended: true })); //content type = application/x-www-form-urlencoded
 
-app.get("/",async (req,res)=>{
-    const allblogs = await blogs.findAll();
-    console.log("allblogs",allblogs)
-    res.render("allBlogs",{allBlogs:allblogs});
-})
+app.get("/", async (req, res) => {
+  const allblogs = await blogs.findAll();
+  //   console.log("allblogs", allblogs);
+  res.render("allBlogs", { allBlogs: allblogs });
+});
 
-app.get("/addBlog",(req,res)=>{
-    res.render("addBlog")
-})
+//get single blog
+app.get("/blogs/:id", async (req, res) => {
+  const _id = req.params.id; // here id means mathi ko :id
 
-//apo for handling formdata
-app.post("/addBlog",upload.single('image'), async (req,res)=>{
-    console.log("API hitted",req.body)
-    console.log("req.file",req.file)
+  //Alternative
+  // const {_id}=req.params; //Object destructuring
+// console.log("blogs",blogs)
+  // fetch/find selected blog's data from table
+  const blog = await blogs.findAll({
+    where: {
+      id: _id
+    }
+  });
 
-    // const title = req.body.title;
-    // const subTitle = req.body.subTitle;
+  // const blog=await allBlogs.findByPk(_id);
+// console.log("blog",blog)
 
-    //ALTERNATIVE
-    const {title,subTitle,description} = req.body;
+  res.render("singleBlog", {blog});
+});
 
-    //  await blogs.create({
-    //     title:req.body.title,
-    //     subTitle:req.body.subtitle,
-    //     description:req.body.description
-    // })
+//delete blog
+app.get("/delete/:id",async (req,res)=>{
+    const _id=req.params.id;
 
-    await blogs.create({
-        title:title,
-        subTitle,
-        description,
-        imageUrl: req.file.filename
+    //delete the selected blog's data row from table
+    await blogs.destroy({
+        where:{
+        id:_id
+        }
     })
-
-    res.send("Blog created successfully")
+    res.redirect("/");
 })
 
+app.get("/addBlog", (req, res) => {
+  res.render("addBlog");
+});
 
-app.use(express.static("./uploads/")) // path should NOT be keep empty otherwise security vulnerable
+//api for handling formdata
+app.post("/addBlog", upload.single("image"), async (req, res) => {
+  //   console.log("API hitted", req.body);
+  //   console.log("req.file", req.file);
 
-const PORT  = process.env.PORT
+  // const title = req.body.title;
+  // const subTitle = req.body.subTitle;
 
-app.listen(PORT,()=>{
-    console.log("Node js project has started at port " + PORT)
-})
+  //ALTERNATIVE
+  const { title, subTitle, description } = req.body;
+
+  //  await blogs.create({
+  //     title:req.body.title,
+  //     subTitle:req.body.subtitle,
+  //     description:req.body.description
+  // })
+
+  await blogs.create({
+    title: title,
+    subTitle,
+    description,
+    imageUrl: req.file.filename,
+  });
+
+  res.redirect("/");
+});
+
+app.use(express.static("./uploads/")); // path should NOT be keep empty otherwise security vulnerable
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  //   console.log("Node js project has started at port " + PORT);
+});
